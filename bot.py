9d362a3725f1d7626de7db66448fbfc7a2cdb4df
mccntprivat-boop <mccntprivat@gmail.com>
2026-08-29 06:12:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import os
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} est connecté !")
+
+@bot.command()
+async def bonjour(ctx):
+    await ctx.send("👋 Bonjour ! Je suis prêt à modérer le serveur.")
+
+bot.run(os.environ["DISCORD_TOKEN"])
